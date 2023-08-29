f4fab79bbbfd03bc058e23b8883e2595ce0e573f
sergivg4 <sergi.valenzuela.externo@penguinrandomhouse.com>
2023-08-29 10:34:33 +0200

    Retrieve Data Extension Rows

--- index.php
+++ index.php
@@ -3,10 +3,29 @@
 require('vendor/autoload.php');
 
 use FuelSdk\ET_Client;
-use FuelSdk\ET_DataExtension_Column;
+use FuelSdk\ET_DataExtension_Row;
 
 $myclient = new ET_Client();
-$dataextensioncolumn = new ET_DataExtension_Column();
-$dataextensioncolumn->authStub = $myclient;
-$response = $dataextensioncolumn->get();
-print_r($response);
+$dataextensionrow = new ET_DataExtension_Row();
+$dataextensionrow->authStub = $myclient;
+$dataextensionrow->Name = 'Test_Rosa';
+$dataextensionrow->props = array('First Name', 'Last Name', 'Email');
+$response = (array) $dataextensionrow->get();
+$response = $response['results'];
+
+echo "<table>";
+echo "<tr>";
+    for ($i=0; $i < 3; $i++) { 
+        echo "<th style='text-align: start;'>" . $response[0]->Properties->Property[$i]->Name . "</th>";
+    }
+echo "</tr>";
+foreach ($response as $key => $value) {
+    foreach ($response[$key]->Properties as $value2) {
+        echo "<tr>";
+        foreach ($value2 as $key => $value3) {
+            echo "<td>" . $value3->Value . "</td>";
+        }
+        echo "</tr>";
+    }
+}
+echo "</table>";
