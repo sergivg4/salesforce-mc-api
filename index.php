2edfc3b39ff5b48b40187c33fd6a63162ffc8ae7
sergivg4 <sergi.valenzuela.externo@penguinrandomhouse.com>
2023-08-29 11:45:49 +0200

    CRUD Marketing Cloud

--- index.php
+++ index.php
@@ -6,21 +6,26 @@ use FuelSdk\ET_Client;
 use FuelSdk\ET_DataExtension_Row;
 
 $myclient = new ET_Client();
+$dataextensionrow = new ET_DataExtension_Row();
+$dataextensionrow->authStub = $myclient;
+$dataextensionrow->Name = 'Semillas_Sindy';
 
-RetrieveDataExtensionRows($myclient);
+// createDataExtensionRows($myclient, $dataextensionrow);
+// updateDataExtensionRows($myclient, $dataextensionrow);
+// deleteDataExtensionRows($myclient, $dataextensionrow);
+retrieveDataExtensionRows($myclient, $dataextensionrow);
 
-function RetrieveDataExtensionRows($myclient) {
+function retrieveDataExtensionRows($myclient, $dataextensionrow) {
 
-    $dataextensionrow = new ET_DataExtension_Row();
     $dataextensionrow->authStub = $myclient;
-    $dataextensionrow->Name = 'Test_Rosa';
-    $dataextensionrow->props = array('First Name', 'Last Name', 'Email');
+    $dataextensionrow->Name = 'Semillas_Sindy';
+    $dataextensionrow->props = array('UUID', 'Email', 'Account ID', 'Birthdate', 'First Name', 'Last Name', 'Mailing City', 'Mailing Country', 'Mailing State', 'Mailing Zip', 'Mobile Phone', 'AgeRange');
     $response = (array) $dataextensionrow->get();
     $response = $response['results'];
 
-    echo "<table>";
+    echo "<table style='width: 100vw;'>";
     echo "<tr>";
-        for ($i=0; $i < 3; $i++) { 
+        for ($i=0; $i < count($dataextensionrow->props); $i++) { 
             echo "<th style='text-align: start;'>" . $response[0]->Properties->Property[$i]->Name . "</th>";
         }
     echo "</tr>";
@@ -34,5 +39,55 @@ function RetrieveDataExtensionRows($myclient) {
         }
     }
     echo "</table>";
+
+}
+
+function createDataExtensionRows($myclient, $dataextensionrow) {
+
+    $dataextensionrow->props = array(
+                            "UUID" => "0031X00503Vv178QAB",
+                            "Email" => "sergivg10@gmail.com",
+                            "Account ID" => "",
+                            "Birthdate" => "7/29/2022 12:00:00 AM",
+                            "First Name" => "Sergi",
+                            "Last Name" => "Valenzuela",
+                            "Mailing City" => "Barcelona",
+                            "Mailing Country" => "ES",
+                            "Mailing State" => "",
+                            "Mailing Zip" => "08291",
+                            "Mobile Phone" => "616616616",
+                            "AgeRange" => "30",
+                        );
+    $results = $dataextensionrow->post();
+    print_r($results);
+
+}
+
+function updateDataExtensionRows($myclient, $dataextensionrow) {
+
+    $dataextensionrow->props = array(
+                            "UUID" => "0031X00503Vv178QAB",
+                            "Email" => "sergivg10000@gmail.com",
+                            "Account ID" => "",
+                            "Birthdate" => "7/29/2022 12:00:00 AM",
+                            "First Name" => "Sergi",
+                            "Last Name" => "Valenzuela",
+                            "Mailing City" => "Barcelona",
+                            "Mailing Country" => "ES",
+                            "Mailing State" => "",
+                            "Mailing Zip" => "08291",
+                            "Mobile Phone" => "616616616",
+                            "AgeRange" => "30",
+                        );
+    $results = $dataextensionrow->patch();
+    print_r($results);
+
+}
+
+function deleteDataExtensionRows($myclient, $dataextensionrow) {
+
+    $dataextensionrow->props = array("UUID" => "0031X00503Vv178QAB");
+    $results = $dataextensionrow->delete();
+    print_r($results);
 
 }
