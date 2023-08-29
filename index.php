bd1de1dae3123ab96d909aeac038a478330c6e4a
sergivg4 <sergi.valenzuela.externo@penguinrandomhouse.com>
2023-08-29 10:38:29 +0200

    RetrieveDataExtensionRows function

--- index.php
+++ index.php
@@ -6,26 +6,33 @@ use FuelSdk\ET_Client;
 use FuelSdk\ET_DataExtension_Row;
 
 $myclient = new ET_Client();
-$dataextensionrow = new ET_DataExtension_Row();
-$dataextensionrow->authStub = $myclient;
-$dataextensionrow->Name = 'Test_Rosa';
-$dataextensionrow->props = array('First Name', 'Last Name', 'Email');
-$response = (array) $dataextensionrow->get();
-$response = $response['results'];
 
-echo "<table>";
-echo "<tr>";
-    for ($i=0; $i < 3; $i++) { 
-        echo "<th style='text-align: start;'>" . $response[0]->Properties->Property[$i]->Name . "</th>";
-    }
-echo "</tr>";
-foreach ($response as $key => $value) {
-    foreach ($response[$key]->Properties as $value2) {
-        echo "<tr>";
-        foreach ($value2 as $key => $value3) {
-            echo "<td>" . $value3->Value . "</td>";
+RetrieveDataExtensionRows($myclient);
+
+function RetrieveDataExtensionRows($myclient) {
+
+    $dataextensionrow = new ET_DataExtension_Row();
+    $dataextensionrow->authStub = $myclient;
+    $dataextensionrow->Name = 'Test_Rosa';
+    $dataextensionrow->props = array('First Name', 'Last Name', 'Email');
+    $response = (array) $dataextensionrow->get();
+    $response = $response['results'];
+
+    echo "<table>";
+    echo "<tr>";
+        for ($i=0; $i < 3; $i++) { 
+            echo "<th style='text-align: start;'>" . $response[0]->Properties->Property[$i]->Name . "</th>";
         }
-        echo "</tr>";
+    echo "</tr>";
+    foreach ($response as $key => $value) {
+        foreach ($response[$key]->Properties as $value2) {
+            echo "<tr>";
+            foreach ($value2 as $key => $value3) {
+                echo "<td>" . $value3->Value . "</td>";
+            }
+            echo "</tr>";
+        }
     }
+    echo "</table>";
+
 }
-echo "</table>";
